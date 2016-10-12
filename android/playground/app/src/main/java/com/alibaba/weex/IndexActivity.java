6ce3430630aa3e7f089c19edd3fb792ba7ecb17b
xkli <569664668@qq.com>
2016-10-12 20:33:44 +0800

    * [android] update modify IP

--- android/playground/app/src/main/java/com/alibaba/weex/IndexActivity.java
+++ android/playground/app/src/main/java/com/alibaba/weex/IndexActivity.java
@@ -35,7 +35,7 @@ public class IndexActivity extends AbstractWeexActivity {
   private static final String TAG = "IndexActivity";
   private static final int CAMERA_PERMISSION_REQUEST_CODE = 0x1;
   private static final String DEFAULT_IP = "your_current_IP";
-  private static String sCurrentIp = "30.10.96.149"; // your_current_IP
+  private static String sCurrentIp = DEFAULT_IP; // your_current_IP
 
   private ProgressBar mProgressBar;
   private TextView mTipView;
