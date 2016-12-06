aacc4be44d81cb1c1fe352a61e4179619985f672
chuyi <jianjian.sjj@alibaba-inc.com>
2016-12-06 10:41:49 +0800

    add ex module

---
+++ android/sdk/src/main/java/com/taobao/weex/ui/module/WXExpressionBindingModule.java
@@ -0,0 +1,15 @@
+package com.taobao.weex.ui.module;
+
+import com.taobao.weex.common.WXModule;
+
+/**
+ * Description:
+ *
+ * Created by rowandjj(chuyi)<br/>
+ */
+
+public class WXExpressionBindingModule extends WXModule{
+
+
+
+}
