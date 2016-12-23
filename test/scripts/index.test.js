72ac973a0903004b7a79035442f128b7412a59d9
sospartan <sospartan@gmail.com>
2016-12-23 18:06:41 +0800

    * [test] code cleanup

--- test/scripts/index.test.js
+++ test/scripts/index.test.js
@@ -7,9 +7,7 @@ var wd = weex.wd
 
 describe('weex mobile index', function () {
   this.timeout(5 * 60 * 1000);
-
   var driver = wd.initPromiseChain();
-
   var windowSize = driver.getWindowSize()
   driver.configureHttp({
     timeout: 100000
@@ -18,47 +16,46 @@ describe('weex mobile index', function () {
   before(function () {
     try {
       return driver
-      .initDriver()
-      .wGet("index.js")
-      .catch()
-      .then()
-      .sleep(2000);
+        .initDriver()
+        .wGet("index.js")
+        .catch()
+        .then()
+        .sleep(2000);
     } catch (error) {
     }
-    
+
   });
 
 
   it('#1 Index', () => {
     return driver
-    .textOfXPath("//div[1]/text[1]")
-    .then((text)=>{
-      assert.equal(text,"hello world.")
-    })
+      .textOfXPath("//div[1]/text[1]")
+      .then((text) => {
+        assert.equal(text, "hello world.")
+      })
   })
 
 
   it('#2 Click Button', () => {
     return driver
-    .wElement("//div[1]/text[3]")
-    .click()
-    .textOfXPath("//div[1]/text[2]")
-    .then((text)=>{
-      assert.equal(text,"btn click.")
-    })
+      .wElement("//div[1]/text[3]")
+      .click()
+      .textOfXPath("//div[1]/text[2]")
+      .then((text) => {
+        assert.equal(text, "btn click.")
+      })
   })
 
   it('#2 Input Blur', () => {
     return driver
-    .wElement("//div[1]/input")
-    .click()
-    .wElement("//div[1]/text[4]")
-    .click()
-    .textOfXPath("//div[1]/text[2]")
-    .then((text)=>{
-      assert.equal(text,"input blur.")
-    })
+      .wElement("//div[1]/input")
+      .click()
+      .wElement("//div[1]/text[4]")
+      .click()
+      .textOfXPath("//div[1]/text[2]")
+      .then((text) => {
+        assert.equal(text, "input blur.")
+      })
   })
- 
-  
+
 });
